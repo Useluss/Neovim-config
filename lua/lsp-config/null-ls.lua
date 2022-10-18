5c4cd2400fd2d6c0dded15450e41ec1543ad6bd0
Ryan Fiset <ryanfiset256@gmail.com>
2022-10-17 19:53:53 -0700

    add spellcheck with null-ls

--- lua/lsp-config/null-ls.lua
+++ lua/lsp-config/null-ls.lua
@@ -3,7 +3,9 @@ local null_ls = require('null-ls')
 local formatting = null_ls.builtins.formatting
 
 local sources = {
-    formatting.rustfmt
+    formatting.rustfmt,
+    null_ls.builtins.diagnostics.cspell, 
+    null_ls.builtins.code_actions.cspell
 }
 
 null_ls.setup({
