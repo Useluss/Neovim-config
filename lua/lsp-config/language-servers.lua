aa48c45355d9b68e6309c4f3a6c24b9ec4d0a458
Ryan Fiset <ryanfiset256@gmail.com>
2022-10-22 10:57:52 -0700

    add LaTeX language server

--- lua/lsp-config/language-servers.lua
+++ lua/lsp-config/language-servers.lua
@@ -1,4 +1,4 @@
-nvim_lsp = require("lspconfig")
+local nvim_lsp = require("lspconfig")
 local mason = require("mason")
 mason.setup({
     ui = {
@@ -14,19 +14,19 @@ require("mason-lspconfig").setup()
 vim.g.mapleader = ' '
 
 local on_attach = function(client, bufnr)
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings.
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format { async = true }, bufopts)
+    -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- 
+    -- -- Mappings.
+    -- local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    -- vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format { async = true }, bufopts)
 end
 
 local lsp_flags = {
@@ -55,4 +55,10 @@ nvim_lsp['sumneko_lua'].setup {
             globals = { "vim" }
         }
     }
+}
+
+nvim_lsp['ltex'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+    capabilities = capabilities,
 }
